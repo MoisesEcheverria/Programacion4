33ebda3397f54adeff544a755c0bf0443921f99e
Moisés Echeverría <lolxd.xdlol57@gmail.com>
2020-09-06 22:30:38 -0300

    diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,7 @@
+diccionario = {'nombre': ['Moises','Kriss','Hector','Pablo'], 'apellido': ['Echeverría','Brignardello','Barraza','Garrido'],'cursos': ['Programacion', 'fisica'], 'nota programacion': [62,70,52,69], 'nota fisica': [56,10,40,70]}
+print(diccionario['nombre'][0])
+print(diccionario['apellido'][0])
+print(diccionario['cursos'][0])
+print(diccionario['nota programacion'][0])
+print(diccionario['cursos'][1])
+print(diccionario['nota fisica'][0])
