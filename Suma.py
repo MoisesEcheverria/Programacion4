7a682acfa9da9d46f1f1f7ebcb76b75276240c41
Moisés Echeverría <lolxd.xdlol57@gmail.com>
2020-09-06 21:56:17 -0300

    Suma.py

---
+++ Suma.py
@@ -0,0 +1,7 @@
+x=5
+y=3
+print(f'La suma se define como {x}+{y}={x+y}')
+print(f'La resta se define como {x}-{y}={x-y}')
+print(f'La multiplicación se define como {x}*{y}={x*y}')
+print(f'La división se define como {x}/{y}={x/y}')
+print(f'El modulo se define como {x}%{y}={x%y}')
